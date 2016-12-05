02e5aca95905b47a90e2b4ce178058c9bbed21ba
Braden Amorozo <braden808@gmail.com>
2016-12-05 08:39:25 -1000

    update

---
+++ javascript.js
@@ -0,0 +1,24 @@
+/*
+Braden Amorozo
+Arrays and Objects
+Dec. 5, 2016
+*/
+
+// variables
+//ARRAYS
+var numbers = [1,2,3,4,5,6,7,8,9,10];
+var names = ["Senior Tan", "Zak", "Yolo Baggins", "Ohai", "Basic Jeff"];
+var colors = ["light black", "ron burgandy", "highlighter"];
+var adjectives = ["smart", "special case", "yummy", "tiny whiny", "ferocious"];
+var slang = ["scrap", "kobe beef", "da kine", "li dat", "pau hana"];
+
+//obects
+
+var donut = {
+	toppings : ["sprikles", "Chocolate Frosting", "o'hare air"],
+	filling : "jelly",
+	shape : "square",
+	count : 12
+
+}
+
